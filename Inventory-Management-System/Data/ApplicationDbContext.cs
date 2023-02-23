5edd9b65664f9abe3fce747efba141d77899529f
Yumit Demir <yumitdemir@outlook.com>
2023-02-23 17:15:09 +0200

    connect the database and initial migration

---
+++ Inventory-Management-System/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_Management_System.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //! public DbSet<RowClassName> ColumnClassName { get; set; }  // CloumnClassName = Dataset Column
+    }
+}
--- Inventory-Management-System/Program.cs
+++ Inventory-Management-System/Program.cs
@@ -1,7 +1,18 @@
+using Inventory_Management_System.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//! Database connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+});
+
+
 
 var app = builder.Build();
 
