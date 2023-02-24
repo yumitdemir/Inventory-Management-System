4ba10d72a8ccb1236a04f67450ce615c4a8d7bd1
Yumit Demir <yumitdemir@outlook.com>
2023-02-24 19:48:52 +0200

    Show product,supplier and category on a html table

--- Inventory-Management-System/Models/Supplier.cs
+++ Inventory-Management-System/Models/Supplier.cs
@@ -6,7 +6,7 @@ namespace Inventory_Management_System.Models
     {
         [Key]
         public int SupplierId { get; set; }
-
+        public string? Name { get; set; }
         public string? Address { get; set; }
         public string? Phone { get; set; }
         public string? Email { get; set; }
